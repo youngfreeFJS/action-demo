7745d6e045f6adcc42a936480f859bc503d721ee
yangfei28 <yangfei28@meituan.com>
2023-02-08 20:19:02 +0800

    t

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 const core = require('@actions/core')
 
 async function run() {
-  const name = core.getInput('name')
-  core.info(`input name is : ${name}`)
-  const envs = process.env
-  for (const env in envs) {
-    if (Object.hasOwnProperty.call(envs, env)) {
-      core.info(`[${env}]: [${envs[env]}]`)
-    }
-  }
-  core.info(`process envs: \n`)
+  core.info(`input name is : ${core.getInput('name')}`)
+  core.info(`human is : ${core.getInput('human')}`)
+  
   
   core.setOutput('age', `${name}/100`)
 }
