51b493ee6b27bc2e4ae88548e6f921aedab6012b
yangfei28 <yangfei28@meituan.com>
2023-02-08 19:31:55 +0800

    Initial project

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const core = require('@actions/core')
 async function run() {
   const name = core.getInput('name')
   core.info(`input name is : ${name}`)
+  core.info(`process envs: \n${process.env}`)
   core.setOutput('age', `${name}/100`)
 }
 
