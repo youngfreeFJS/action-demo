74f9abafe9c4af5b25eea918a41d77e5658704f7
yangfei28 <yangfei28@meituan.com>
2023-02-08 20:24:53 +0800

    Initial project

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const core = require('@actions/core')
 async function run() {
   core.info(`input name is : ${core.getInput('name')}`)
   core.info(`human is : ${core.getInput('human')}`)
+  core.info(`not_required is : ${core.getInput('not_required')}`)
+  core.info(`job is : ${core.getInput('job')}`)
   
   
   core.setOutput('age', `${name}/100`)
