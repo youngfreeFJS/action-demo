6ebd4ea61f3a591caafec52bcc2102ca1dd74db0
yangfei28 <yangfei28@meituan.com>
2023-02-08 19:37:35 +0800

    Initial project

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const core = require('@actions/core')
 async function run() {
   const name = core.getInput('name')
   core.info(`input name is : ${name}`)
-  core.info(`process envs: \n${process.env}`)
+  const envs = process.env
+  for (const env in envs) {
+    if (Object.hasOwnProperty.call(envs, env)) {
+      core.info(`[${env}]: [${envs[env]}]`)
+    }
+  }
+  core.info(`process envs: \n`)
+  
   core.setOutput('age', `${name}/100`)
 }
 
